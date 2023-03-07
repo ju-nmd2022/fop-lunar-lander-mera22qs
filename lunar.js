27522a2283f850d5cd3bb6a5c182f75b2f491925
mera22qs <rame22qs@student.ju.se>
2023-03-07 15:39:35 +0100

    Trying to solve problems

--- lunar.js
+++ lunar.js
@@ -2,48 +2,8 @@ function setup() {
   createCanvas(800, 500);
 }
 
-function nature() {
-  background(212, 126, 151);
-  push();
-  
-  /*Sky ellipses*/
-  fill(206, 141, 162);
-  noStroke();
-  ellipse(400, 150, 800, 250);
-  
-  fill(201, 151, 169);
-  noStroke();
-  ellipse(400, 150, 600, 200);
-  
-  /*Mountains*/
-  fill(135, 127, 168);
-  stroke(112, 103, 165);
-  triangle(550, 301, 674, 130, 800, 301);
-  fill(255, 255, 255);
-  stroke(255, 255, 255);
-  triangle(634, 184, 674, 130, 714, 184);
-  
-  fill(135, 127, 168);
-  stroke(112, 103, 165);
-  triangle(-2, 300, 141, 61, 282, 301);
-  fill(255, 255, 255);
-  stroke(255, 255, 255);
-  triangle(105, 118, 141, 62, 174, 118);
-  
-  fill(135, 127, 168);
-  stroke(112, 103, 165);
-  triangle(231, 301, 412, 2, 591, 301);
-  fill(255, 255, 255);
-  stroke(255, 255, 255);
-  triangle(375, 61, 412, 2, 449, 61);
-  
-  fill(151, 198, 159);
-  noStroke();
-  rect(0, 300, 800, 500);
-  
-  pop();
-}
-
+let trampolineX = 300;
+let trampolineY = 400;
 
 function trampoline(trampolineX, trampolineY) {
   push();
@@ -87,6 +47,48 @@ function bunny(bunnyX, bunnyY, rotation) {
   
   bunnyY = bunnyY - 3;
 }
+function nature() {
+  background(212, 126, 151);
+  push();
+  
+  /*Sky ellipses*/
+  fill(206, 141, 162);
+  noStroke();
+  ellipse(400, 150, 800, 250);
+  
+  fill(201, 151, 169);
+  noStroke();
+  ellipse(400, 150, 600, 200);
+  
+  /*Mountains*/
+  fill(135, 127, 168);
+  stroke(112, 103, 165);
+  triangle(550, 301, 674, 130, 800, 301);
+  fill(255, 255, 255);
+  stroke(255, 255, 255);
+  triangle(634, 184, 674, 130, 714, 184);
+  
+  fill(135, 127, 168);
+  stroke(112, 103, 165);
+  triangle(-2, 300, 141, 61, 282, 301);
+  fill(255, 255, 255);
+  stroke(255, 255, 255);
+  triangle(105, 118, 141, 62, 174, 118);
+  
+  fill(135, 127, 168);
+  stroke(112, 103, 165);
+  triangle(231, 301, 412, 2, 591, 301);
+  fill(255, 255, 255);
+  stroke(255, 255, 255);
+  triangle(375, 61, 412, 2, 449, 61);
+  
+  fill(151, 198, 159);
+  noStroke();
+  rect(0, 300, 800, 500);
+  
+  pop();
+}
+
 
 
 function butn() {
@@ -182,12 +184,6 @@ function gameScr() {
 
 function resultScr() {
   nature();
-
-  if(----) {
-    win();
-  } else {
-    fail();
-  }
 }
 
 let state = "start";
